a4a62009002762b80ab602106353794be4a50b3c
Sasha Kavun <iam@sashakavun.ru>
2014-01-29 00:07:25 +0400

    Initial version.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-leaflet-canvasicon
-==================
+L.CanvasIcon
+============
 
-Canvas Icon plugin for Leaflet library
+Canvas Icon plugin for Leaflet library.
+
+Check out code and the <a href="http://keta.github.io/leaflet-canvasicon/example.html">example</a>.
---
+++ leaflet-canvasicon.js
@@ -0,0 +1,74 @@
+/*global L */
+(function () {
+    'use strict';
+
+    /**
+     * Canvas Icon
+     * @type {L.CanvasIcon}
+     * @extends {L.Icon}
+     */
+    L.CanvasIcon = L.Icon.extend({
+        options: {
+            /** @var {L.Point} */
+            iconSize: [24, 24],
+            /** @var {L.Point} */
+            iconAnchor: [12, 12],
+            /** @var {function} */
+            iconStyles: null,
+            /* @var {string} */
+            className: 'leaflet-canvas-icon'
+        },
+
+        /**
+         * @param {HTMLElement} oldIcon
+         * @returns {HTMLCanvasElement}
+         */
+        createIcon: function (oldIcon) {
+            console.log('create icon');
+            var size = L.point(this.options.iconSize);
+            var icon = (oldIcon && (oldIcon.tagName == 'CANVAS')) ? oldIcon : document.createElement('canvas');
+            icon.width = size.x;
+            icon.height = size.y;
+            this._setIconStyles(icon, 'icon');
+            return icon;
+        },
+
+        /**
+         * @param {HTMLElement} oldIcon
+         * @returns {null}
+         */
+        createShadow: function (oldIcon) {
+            return null;
+        },
+
+        /**
+         * @param {HTMLElement} icon
+         * @param {string} type
+         * @private
+         */
+        _setIconStyles: function (icon, type) {
+            console.log('icon styles', arguments);
+            if (typeof this.options.iconStyles == 'function') {
+                this.options.iconStyles.apply(this, arguments);
+            }
+            L.Icon.prototype._setIconStyles.apply(this, arguments);
+        }
+    });
+
+    /**
+     * Canvas Icon factory
+     * @param {object} options
+     * @returns {L.CanvasIcon}
+     */
+    L.canvasIcon = function (options) {
+        return new L.CanvasIcon(options);
+    };
+
+    /**
+     * AMD compatibility
+     */
+    if ((typeof define == 'function') && define.amd) {
+        define(L.CanvasIcon);
+    }
+
+}());
